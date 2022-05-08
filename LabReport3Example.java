60bb30758adb19544c3ded0386211ad7a0502bdd
JZ567 <jzwiebel@ucsd.edu>
2022-05-08 15:46:10 -0700

    Practice commit
    Practice commit

--- LabReport3Example.java
+++ LabReport3Example.java
@@ -1,4 +1,5 @@
 public class LabReport3Example {
+  // editing code to test Github access from ieng6
     public static void main(String[] args) {
         System.out.println(System.getProperty("os.name"));
         System.out.println(System.getProperty("user.name"));
